b8442c95adaf3b6d1a62b9e669985babeb2a3801
Ssurey Moon <ssureymoon@gmail.com>
2025-07-12 22:06:10 +0900

    fix product creation issue

--- domain/src/main/kotlin/com/simple/domain/entity/BrandEntity.kt
+++ domain/src/main/kotlin/com/simple/domain/entity/BrandEntity.kt
@@ -2,6 +2,8 @@ package com.simple.domain.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 
@@ -9,6 +11,7 @@ import jakarta.persistence.Table
 @Table(name = "brands")
 data class BrandEntity(
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     val id: Long? = null,
 
--- domain/src/main/kotlin/com/simple/domain/entity/ProductEntity.kt
+++ domain/src/main/kotlin/com/simple/domain/entity/ProductEntity.kt
@@ -2,6 +2,8 @@ package com.simple.domain.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Index
 import jakarta.persistence.Table
@@ -10,6 +12,7 @@ import jakarta.persistence.Table
 @Table(name = "products", indexes = [Index(name = "idx_category_price", columnList = "category, price")])
 data class ProductEntity(
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     val id: Long? = null,
 
